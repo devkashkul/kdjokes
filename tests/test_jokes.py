cc79625ef5299dde87159b0e786acbe1ee3b7dba
Kashkul Dev <devkashkul@gmail.com>
2024-11-23 23:51:57 +0300

    make error

--- tests/test_jokes.py
+++ tests/test_jokes.py
@@ -2,7 +2,7 @@ from main import get_random_joke
 
 def test_joke_length():
     joke = get_random_joke()
-    assert len(joke) > 30
+    assert len(joke) < 1
 
 def test_joke_word_length():
     joke = get_random_joke()
